1f950ea6012d959995bc441d2b8b8187e2a060d1
UjalaJha <jhaujala3@gmail.com>
2020-01-17 19:32:12 +0530

    fixed login

--- Web/app/Http/Controllers/LoginController.php
+++ Web/app/Http/Controllers/LoginController.php
@@ -31,10 +31,14 @@ class LoginController extends Controller
 
     public function login_check()
     {
-
+        if(empty($_POST['email'])){
+            echo "invalid";
+            return view('landing/login');
+            exit();
+        }
         $credentials = Login::where('email_id', $_POST['email'])->get();
 //        echo "<pre>";
-        if ($credentials[0]->password == $_POST['password']) {
+        if (!empty($credentials) && $credentials[0]->password == $_POST['password']) {
 //            echo "valid";
             $uid = $credentials[0]->user_id;
 //            echo $uid;
@@ -44,6 +48,8 @@ class LoginController extends Controller
             return view('template/dashboard');
         } else {
             echo "invalid";
+            return view('landing/login');
+            exit();
         }
 
     }
