538e5f44f083b93d9aea770306705a733b9f1929
UjalaJha <jhaujala3@gmail.com>
2020-07-30 15:23:29 +0530

    blog recommendation added

--- Web/app/Http/Controllers/UserController.php
+++ Web/app/Http/Controllers/UserController.php
@@ -642,6 +642,38 @@ class UserController extends Controller
         echo $response;
 
     }
+    public function blogrecommendation(){
+
+        $data['name']='Maya';
+        $data['skills']='python,graphql,chatbot,bootstrap,finance,angularjs,machine learning,ai,rest';
+        $json=json_encode($data);
+        // print_r($json);
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+          CURLOPT_URL => "http://127.0.0.1:5000/blog_api",
+          CURLOPT_RETURNTRANSFER => true,
+          CURLOPT_ENCODING => "",
+          CURLOPT_MAXREDIRS => 10,
+          CURLOPT_TIMEOUT => 0,
+          CURLOPT_FOLLOWLOCATION => true,
+          CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+          CURLOPT_CUSTOMREQUEST => "POST",
+          CURLOPT_POSTFIELDS =>$json,
+          CURLOPT_HTTPHEADER => array(
+            "Content-Type: application/json"
+          ),
+        ));
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+        echo "<pre>";
+        print_r(json_decode($response));
+
+
+    }
+
     public function store(Request $request)
     {
         //
