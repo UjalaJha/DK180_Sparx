1679458de261dd2f24dea471a34f34aaf7cf50ca
Khushboo <2016.khushboo.chandnani@ves.ac.in>
2020-08-02 17:00:48 +0530

    Admin icon fixes

--- Web/resources/views/includes/admin_sidebar.blade.php
+++ Web/resources/views/includes/admin_sidebar.blade.php
@@ -54,13 +54,13 @@
           </li>
            <li class="nav-item ">
             <a class="nav-link" href="aicte">
-              <i class="material-icons">playlist_add</i>
+              <i class="material-icons">assessment</i>
               <p>AICTE</p>
             </a>
           </li>
            <li class="nav-item ">
             <a class="nav-link" href="learning_platform">
-              <i class="material-icons">playlist_add</i>
+              <i class="material-icons">insights</i>
               <p>Learning Platform Insights</p>
             </a>
           </li>
