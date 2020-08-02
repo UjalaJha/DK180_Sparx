2afcd13270e4b2cd54331deeb222cdf6c4dda3ab
Khushboo <2016.khushboo.chandnani@ves.ac.in>
2020-08-02 11:50:00 +0530

    UI fixes

--- Web/resources/views/template/tq_test.blade.php
+++ Web/resources/views/template/tq_test.blade.php
@@ -18,11 +18,10 @@
 
 <head>
   <meta charset="utf-8" />
-  <link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../../assets/img/favicon.png">
+  <link rel="icon" href="../images/top-favicon.png" type="image/png" sizes="16x26">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
-    Material Dashboard by Creative Tim
+    HireConnect - Advocating happy careers - Job Recommender System
   </title>
   <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
   <!--     Fonts and icons     -->
